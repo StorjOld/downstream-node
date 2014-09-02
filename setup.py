9048b976f68fac971df4eb5651785872a7a42f61
Paul Durivage <pauldurivage@gmail.com>
2014-09-02 15:01:59 -0500

    Initial commit of project skel

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='downstream-node',
+    version='',
+    packages=['downstream-node'],
+    url='',
+    license='',
+    author='Storj Labs',
+    author_email='info@storj.io',
+    description=''
+)
