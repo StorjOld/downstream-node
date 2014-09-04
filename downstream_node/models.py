3e42af8ac949032d8dc2c4bc181a64fc2fbed651
Paul Durivage <pauldurivage@gmail.com>
2014-09-03 21:22:17 -0500

    Add model stuff into DB

--- downstream_node/models.py
+++ downstream_node/models.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from downstream_node.startup import db
 
-from sqlalchemy import Table
+
+class Files(db.Model):
+    __tablename__ = 'files'
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    filepath = db.Column('filepath', db.String())
+
+
+class Challenges(db.Model):
+    __tablename__ = 'challenges'
+
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    filepath = db.Column(db.ForeignKey('files.filepath'))
+    block = db.Column('block', db.String())
+    seed = db.Column('seed', db.String())
+    response = db.Column('response', db.String(), nullable=True)
