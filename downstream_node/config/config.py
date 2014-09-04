24560eb0143ff26aa46e64393fe6d6423958771d
Paul Durivage <pauldurivage@gmail.com>
2014-09-03 21:22:08 -0500

    Add heartbeat secret into config

--- downstream_node/config/config.py
+++ downstream_node/config/config.py
@@ -8,3 +8,9 @@ SECRET_KEY = os.urandom(32)
 
 # SQLAlchemy (DB)
 SQLALCHEMY_DATABASE_URI = 'mysql://localhost'
+
+# Heartbeat
+HEARTBEAT_SECRET = (
+    r'6\x1eg\xd4\x19\xde\xad\xc1x\x00+\xc9\x04~_`%\x'
+    r'f0\x7fF\xd9\x0b=\x91J\xe5\x0b\xeb\xc1D\xcd\x8d'
+)
