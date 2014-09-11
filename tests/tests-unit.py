598caee2b2e68056b32b5c71254f967351925cb7
Paul Durivage <pauldurivage@gmail.com>
2014-09-10 21:29:33 -0500

    Add beginning of unittests

---
+++ tests/tests-unit.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+
+import unittest
+
+from downstream_node.startup import app
+
+
+class TestDownstream(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        app.config['TESTING'] = True
+
+    def tearDown(self):
+        del self.app
+
+    def test_api_downstream_challenge(self):
+        r = self.app.get('/api/downstream/challenges/test')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(r.content_type, 'application/json')
+
+        r_json = json.loads(r.data)
+        self.assertIsInstance(r_json, dict)
+        self.assertEqual(len(r_json.get('challenges')), 1000)
+        self.assertEqual(r_json.get('challenges')[0].get('filename'), 'thirty-two_meg.testfile')
+
+    def test_api_downstream_challenge_answer(self):
+        r = self.app.get('api/downstream/challenges/answer/test')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
