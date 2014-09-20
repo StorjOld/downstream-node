44a81ef1f7b6db5d6f8a45cefa72a0cd92971524
Paul Durivage <pauldurivage@gmail.com>
2014-09-20 18:32:06 -0500

    Complete tests

--- tests/tests-unit.py
+++ tests/tests-unit.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import json
+import os
 
 import unittest
+from heartbeat import Heartbeat
+
 from downstream_node.startup import app, db
 
 from downstream_node import models
+from downstream_node.lib import node, utils
 
 
 class TestDownstreamRoutes(unittest.TestCase):
@@ -93,12 +97,67 @@ class TestDownstreamModels(unittest.TestCase):
         self.assertTrue(hasattr(models.Challenges, 'seed'))
         self.assertTrue(hasattr(models.Challenges, 'response'))
 
+
 class TestDownstreamNodeFuncs(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.testfile = os.path.abspath('tests/thirty-two_meg.testfile')
+
+    def tearDown(self):
+        pass
+
+    def test_create_token(self):
+        with self.assertRaises(NotImplementedError):
+            node.create_token()
+
+    def test_delete_token(self):
+        with self.assertRaises(NotImplementedError):
+            node.delete_token()
+
+    def test_add_file(self):
+        with self.assertRaises(NotImplementedError):
+            node.add_file()
+
+    def test_remove_file(self):
+        with self.assertRaises(NotImplementedError):
+            node.remove_file()
+
+    def test_gen_challenges(self):
+        db.create_all()
+        node.gen_challenges(self.testfile, 'test root seed')
+        challenges = models.Challenges.query.all()
+        db.session.close()
+        db.engine.execute('DROP TABLE challenges,files')
+        self.assertEqual(len(challenges), 1000)
+
+    def test_update_challenges(self):
+        with self.assertRaises(NotImplementedError):
+            node.update_challenges()
 
 
 class TestDownstreamUtils(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.testfile = os.path.abspath('tests/thirty-two_meg.testfile')
+        self.app = app.test_client()
+        app.config['TESTING'] = True
+        db.create_all()
+
+    def tearDown(self):
+        db.session.close()
+        db.engine.execute('DROP TABLE challenges,files')
+        del self.app
+
+    def test_query_to_list(self):
+        node.gen_challenges(self.testfile, 'test root seed')
+        result = utils.query_to_list(models.Challenges.query)
+        self.assertIsInstance(result, list)
+        [self.assertIsInstance(item, dict) for item in result]
+
+    def test_load_heartbeat(self):
+        test_hb = Heartbeat(self.testfile, 'test secret')
+        node.gen_challenges(self.testfile, 'test root seed')
+        result = models.Challenges.query.all()
+        hb = utils.load_heartbeat(test_hb, result)
+        self.assertEqual(len(hb.challenges), 1000)
 
 
 if __name__ == '__main__':
