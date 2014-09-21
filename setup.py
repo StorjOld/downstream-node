24a6ff064036248043ff609ec7ba1925832219c4
Paul Durivage <pauldurivage@gmail.com>
2014-09-21 12:52:04 -0500

    Add proper dependency links and install_requires lines for Py3 and Py2 support

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
+import sys
 from setuptools import setup
 
 from downstream_node import __version__
+
+# Reqirements for all versions of Python
+install_requires = [
+    'flask',
+    'pymysql',
+    'flask-sqlalchemy',
+    'heartbeat==0.1.2',
+]
+
+# Requirements for Python 2
+if sys.version_info < (3,):
+    extras = [
+        'mysql-python',
+    ]
+    install_requires.extend(extras)
 
 setup(
     name='downstream-node',
@@ -11,13 +27,8 @@ setup(
     author='Storj Labs',
     author_email='info@storj.io',
     description='',
-    install_requires=[
-        'flask',
-        'mysql-python',
-        'flask-sqlalchemy',
-        'heartbeat==0.1.2',
-    ],
+    install_requires=install_requires,
     dependency_links=[
-        'https://github.com/Storj/heartbeat/archive/v0.1.2.tar.gz#egg=heartbeat-0.1.2'
+        'git+https://github.com/Storj/heartbeat.git@v0.1.2#egg=heartbeat-0.1.2'
     ],
 )
