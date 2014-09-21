a5a6aa2959ae4db93eb3d179419a40d369bff743
Paul Durivage <pauldurivage@gmail.com>
2014-09-21 10:44:20 -0500

    add heartbeat to the setup.py requirements, hardcode to a TAG RELEASE

--- setup.py
+++ setup.py
@@ -15,5 +15,9 @@ setup(
         'flask',
         'mysql-python',
         'flask-sqlalchemy',
-    ]
+        'heartbeat==0.1.2',
+    ],
+    dependency_links=[
+        'https://github.com/Storj/heartbeat/archive/v0.1.2.tar.gz#egg=heartbeat-0.1.2'
+    ],
 )
