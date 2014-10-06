99b868992eb30e833c8534afb128d9ef392fc337
Will James <jameswt@gmail.com>
2014-10-07 10:55:36 +1100

    fix printing bug.

--- downstream_node/routes.py
+++ downstream_node/routes.py
@@ -44,7 +44,7 @@ def api_downstream_chunk_contract(token):
         # now since we are prototyping, we can delete the tag and file
         print('removing {0}'.format(db_contract.file.path))
         os.remove(db_contract.file.path)
-        print('removing {1}'.format(db_contract.tag_path))
+        print('removing {0}'.format(db_contract.tag_path))
         os.remove(db_contract.tag_path)
 
         return jsonify(seed=db_contract.seed,
